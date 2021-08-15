f2cb4e48d64523dac5f34500b65091b77cc20487
Robert Forkel <xrotwang@googlemail.com>
2021-08-15 18:42:40 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cldfzenodo',
-    version='0.1.0',
+    version='0.1.1.dev0',
     author='Robert Forkel',
     author_email='dlce.rdm@eva.mpg.de',
     description='functionality to retrieve CLDF datasets deposited on Zenodo',
