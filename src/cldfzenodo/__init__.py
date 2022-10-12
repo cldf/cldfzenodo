d533be125559d10dfc53de60dc3b8eb5fe0caebe
Robert Forkel <xrotwang@googlemail.com>
2022-10-12 17:37:28 +0200

    bump version for development

--- src/cldfzenodo/__init__.py
+++ src/cldfzenodo/__init__.py
@@ -4,7 +4,7 @@ from cldfzenodo.record import Record
 from cldfzenodo import oai
 from cldfzenodo import search
 
-__version__ = '0.3.0'
+__version__ = '0.3.1.dev0'
 
 assert Record
 search_wordlists = functools.partial(search.iter_records, 'cldf:Wordlist')
