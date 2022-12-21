6e10770a603e9ebefa08796d2dba17a209a3fcf7
Robert Forkel <xrotwang@googlemail.com>
2022-12-21 10:20:21 +0100

    release 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changes
 
 
-## [Unreleased]
+## [1.1.0] - 2022-12-21
 
 - Added support for retrieval of versioned datasets via concept DOI and version tag.
 - Added support for downloading full deposits via CLI.
--- src/cldfzenodo/__init__.py
+++ src/cldfzenodo/__init__.py
@@ -4,7 +4,7 @@ from cldfzenodo.record import Record
 from cldfzenodo import oai
 from cldfzenodo import search
 
-__version__ = '1.0.1.dev0'
+__version__ = '1.1.0'
 
 assert Record
 search_wordlists = functools.partial(search.iter_records, 'cldf:Wordlist')
