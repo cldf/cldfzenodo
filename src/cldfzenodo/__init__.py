4d9cd3cb1e639faca2812782f11bee8e007a77b2
Robert Forkel <xrotwang@googlemail.com>
2026-05-05 14:54:16 +0200

    bump version for development

--- src/cldfzenodo/__init__.py
+++ src/cldfzenodo/__init__.py
@@ -4,4 +4,4 @@ Functionality to access CLDF datasets on Zenodo programmatically.
 from cldfzenodo.record import *  # noqa: F401,F403
 from cldfzenodo.api import *  # noqa: F401,F403
 
-__version__ = "3.0.0"
+__version__ = "3.0.1.dev0"
